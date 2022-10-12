7fb7e49935abec57814c3105af02c3e34285c2b1
Dang Giang <gianggiang1512@gmail.com>
2022-10-12 22:00:32 +0700

    First commit, add spring integration file dependency

---
+++ src/main/java/com/dtg/SimpleFlowSpringIntegration/SimpleFlowSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dtg.SimpleFlowSpringIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFlowSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFlowSpringIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtg/SimpleFlowSpringIntegration/SimpleFlowSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtg.SimpleFlowSpringIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFlowSpringIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
