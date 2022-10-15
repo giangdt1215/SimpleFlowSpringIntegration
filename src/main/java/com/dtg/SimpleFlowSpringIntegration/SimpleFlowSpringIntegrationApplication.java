4197b0c628b1644dbc1a0a3218740016bfeb458a
GiangDT <giangdt1215@gmail.com>
2022-10-15 16:38:27 +0700

    Add commandlinerunner bean to use FileWriterGateway

--- src/main/java/com/dtg/SimpleFlowSpringIntegration/SimpleFlowSpringIntegrationApplication.java
+++ src/main/java/com/dtg/SimpleFlowSpringIntegration/SimpleFlowSpringIntegrationApplication.java
@@ -1,7 +1,10 @@
 package com.dtg.SimpleFlowSpringIntegration;
 
+import com.dtg.SimpleFlowSpringIntegration.integration.FileWriterGateway;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleFlowSpringIntegrationApplication {
@@ -10,4 +13,11 @@ public class SimpleFlowSpringIntegrationApplication {
 		SpringApplication.run(SimpleFlowSpringIntegrationApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner writeData(FileWriterGateway gateway){
+		return args -> {
+			String myName = "Dang Truong Giang";
+			gateway.writeToFile("giangdt.txt", myName);
+		};
+	}
 }
